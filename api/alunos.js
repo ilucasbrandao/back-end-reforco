f69697166604bb7deebd7a0cd4725b9e92e65eee
ilucasbrandao <dev.lucasbrandao@gmail.com>
2025-09-09 19:55:17 -0300

    ultimo ajuste antes do deploy

--- api/alunos.js
+++ api/alunos.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import serverless from "serverless-http";
-import routeAlunos from "../routes/students.js";
+import routeAlunos from "../routes/students.js"; // 👈 sobe um nível
+import { pool } from "../db.js"; // 👈 sobe um nível
 
 dotenv.config();
 
@@ -10,7 +11,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Usar "/" para evitar /api/alunos/alunos
-app.use("/", routeAlunos);
+app.use("/alunos", routeAlunos);
+
+app.get("/ping", async (req, res) => {
+  try {
+    const conn = await pool.getConnection();
+    const [rows] = await conn.query("SELECT 1");
+    conn.release();
+    res.status(200).send("✅ Conexão com MySQL bem-sucedida!");
+  } catch (err) {
+    console.error("Erro na conexão:", err.message);
+    res.status(500).send("❌ Falha na conexão com o banco de dados.");
+  }
+});
 
 export default serverless(app);
