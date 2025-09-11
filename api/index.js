e0c8dec20a9582d6dc8cd3498815214a591da462
ilucasbrandao <dev.lucasbrandao@gmail.com>
2025-09-11 19:23:44 -0300

    atualização da pasta

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import dotenv from "dotenv";
 import cors from "cors";
 import serverless from "serverless-http";
