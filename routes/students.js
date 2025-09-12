ae5cd62abc8cd08e511e4f9abaa9cbec4b9fe501
ilucasbrandao <dev.lucasbrandao@gmail.com>
2025-09-11 22:53:46 -0300

    more one

--- routes/students.js
+++ routes/students.js
@@ -3,7 +3,7 @@ import express from "express";
 import {
   getStudentsAll,
   getStudentById,
-  createStudent,
+  cadastrar,
 } from "../controllers/students.js";
 
 const router = express.Router();
