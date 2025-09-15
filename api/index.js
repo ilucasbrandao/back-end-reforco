edef2db84246e340bbcc87424af199aeed03fca9
ilucasbrandao <dev.lucasbrandao@gmail.com>
2025-09-15 19:46:40 -0300

    cors global

--- api/index.js
+++ api/index.js
@@ -1,27 +1,30 @@
+// Imports
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import routeAlunos from "../routes/students.js";
 import { pool } from "../db.js";
 
+// Config
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middlewares
 app.use(express.json());
 
-// Configuração CORS
+// Configuração CORS global
 const allowedOrigins = [
   "https://sistema-escolar-juh.vercel.app",
   "https://sistema-escolar-git-main-ilucasbrandaos-projects.vercel.app",
   "https://sistema-escolar-hnpllebk7-ilucasbrandaos-projects.vercel.app",
-  "http://localhost:5173",
+  "http://localhost:5173", // para desenvolvimento local
 ];
 
-// Middleware CORS aplicado **em todas as rotas**
 app.use(
   cors({
     origin: function (origin, callback) {
+      // permite requisições sem origin (ex: Postman)
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
@@ -33,15 +36,6 @@ app.use(
     credentials: true,
   })
 );
-
-// Aplica automaticamente o preflight (OPTIONS) em todas as rotas
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.sendStatus(204);
-  } else {
-    next();
-  }
-});
 
 // Rotas
 app.use("/alunos", routeAlunos);
