4c90c13e24c80c03cf2ee31c746b013a49b611ea
ilucasbrandao <dev.lucasbrandao@gmail.com>
2025-09-15 19:55:08 -0300

    cors simples

--- api/index.js
+++ api/index.js
@@ -1,41 +1,17 @@
-// Imports
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import routeAlunos from "../routes/students.js";
 import { pool } from "../db.js";
 
-// Config
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
 app.use(express.json());
 
-// Configuração CORS global
-const allowedOrigins = [
-  "https://sistema-escolar-juh.vercel.app",
-  "https://sistema-escolar-git-main-ilucasbrandaos-projects.vercel.app",
-  "https://sistema-escolar-hnpllebk7-ilucasbrandaos-projects.vercel.app",
-  "http://localhost:5173", // para desenvolvimento local
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // permite requisições sem origin (ex: Postman)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+// CORS simples: permite qualquer domínio
+app.use(cors());
 
 // Rotas
 app.use("/alunos", routeAlunos);
