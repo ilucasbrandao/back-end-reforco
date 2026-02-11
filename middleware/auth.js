876aea56f144257a2b9db61f10eb242676f2b706
ilucasbrandao <dev.lucasbrandao@gmail.com>
2026-02-11 16:36:33 -0300

    implementação do visto por ultimo no sistema.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 import { verifyToken } from "../utils/jwt.js";
+import prisma from "../prisma.js"; // 1. NÃO ESQUEÇA DE IMPORTAR O PRISMA AQUI
 
 const auth = (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -19,6 +20,17 @@ const auth = (req, res, next) => {
     req.userId = decoded.id;
     req.userEmail = decoded.email;
     req.userRole = decoded.role;
+
+    if (req.userId) {
+      prisma.users
+        .update({
+          where: { id: req.userId },
+          data: { ultimo_acesso: new Date() },
+        })
+        .catch((err) =>
+          console.error("Erro silencioso ao atualizar acesso:", err.message),
+        );
+    }
 
     next();
   } catch (error) {
